29a2e099004008cb392c4f04912c552be24a8e4b
Antony952003 <antonyjeson2003@gmail.com>
2024-04-17 17:04:11 +0530

    Day 7 3 tier Application DAL DLL models

---
+++ Day 7/ClinicAppointmentSolution/ClinicAppointmentBLLibrary/IAppointmentService.cs
@@ -0,0 +1,30 @@
+﻿using ClinicAppointmentModelLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicAppointmentBLLibrary
+{
+    public interface IAppointmentService
+    {
+        List<Appointment> GetAllAppointments();
+
+        Appointment GetAppointmentById(int appointmentId);
+
+        bool IsthereanAppointment(Appointment appointment);
+
+        bool ScheduleAppointment(Appointment appointment);
+
+        Appointment UpdateAppointment(Appointment appointment);
+
+        Appointment CancelAppointment(int appointmentId);
+
+        Appointment RescheduleAppointment(int appointmentId, DateTime newDateTime);
+
+        List<Appointment> GetAppointmentsForDoctor(int doctorId);
+
+        List<Appointment> GetAppointmentsForPatient(int patientId);
+    }
+}
---
+++ Day 7/ClinicAppointmentSolution/ClinicAppointmentBLLibrary/IPatientService.cs
@@ -0,0 +1,20 @@
+﻿using ClinicAppointmentModelLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicAppointmentBLLibrary
+{
+    public interface IPatientService
+    {
+        int AddPatient(Patient patient);
+        Appointment AddAppointmentWithDoctor(Patient patient, Doctor doctor, DateTime newDateTime);
+        Patient DeletePatient(Patient patient);
+        List<Patient> GetAllPatients();
+        Patient UpdatePatient(Patient patient);
+        void CancelAppointmentForPatient(int appointmentId);
+        void RescheduleAppointmentForPatient(int appointmentId, DateTime newDateTime);
+    }
+}
