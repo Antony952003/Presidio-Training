0c154737e34d50cf0c470474fbfd6ccd6693a2da
Antony952003 <antonyjeson2003@gmail.com>
2024-04-19 19:53:05 +0530

    Day 8 Assignment RequestTracker with Employee, Department BL with Repositories

---
+++ Day 8/RequestTracker2Solution/RequestTrackerBlLibrary/EmployeeBL.cs
@@ -0,0 +1,72 @@
+﻿using RequestTrackerDALLibrary;
+using RequestTrakerModelLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RequestTrackerBlLibrary
+{
+    public class EmployeeBL : IEmployeeService
+    {
+        readonly IRepository<int, Employee> _employeeRepository;
+        public EmployeeBL()
+        {
+            _employeeRepository = new EmployeeRepository();
+        }
+
+        int IEmployeeService.AddEmployee(Employee employee)
+        {
+            var result = _employeeRepository.Add(employee);
+            if(result != null)
+            {
+                return result.Id;
+            }
+            throw new DuplicateEmployeeNameException();
+        }
+
+        Employee IEmployeeService.ChangeEmployeeName(string EmployeeOldName, string EmployeeNewName)
+        {
+            var employee = _employeeRepository.GetAll().Find(e => e.Name == EmployeeOldName);
+            if(employee != null)
+            {
+                employee.Name = EmployeeNewName;
+                _employeeRepository.Update(employee);
+                return employee;
+            }
+            throw new EmployeeNotFoundException();
+        }
+
+        Employee IEmployeeService.GetEmployeeById(int id)
+        {
+            var result = _employeeRepository.Get(id);
+            if(result != null)
+            {
+                return result;
+            }
+            throw  new EmployeeNotFoundException();
+        }
+
+        Employee IEmployeeService.GetEmployeeByName(string EmployeeName)
+        {
+            var result = _employeeRepository.GetAll().Find(e => e.Name == EmployeeName);
+            if(result != null)
+            {
+                return result;
+            }
+            throw new EmployeeNotFoundException();
+        }
+
+        int IEmployeeService.GetEmployeeDepartmentId(int EmployeeId)
+        {
+            var result = _employeeRepository.GetAll().Find(e => e.EmployeeDepartment.Id == EmployeeId);
+            if(result != null)
+            {
+                return result.Id;
+            }
+            throw new EmployeeNotFoundException();
+        }
+    }
+}
---
+++ Day 8/RequestTracker2Solution/RequestTrackerBlLibrary/IEmployeeService.cs
@@ -0,0 +1,18 @@
+﻿using RequestTrakerModelLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequestTrackerBlLibrary
+{
+    public interface IEmployeeService
+    {
+        int AddEmployee(Employee employee);
+        Employee ChangeEmployeeName(string EmployeeOldName, string EmployeeNewName);
+        Employee GetEmployeeById(int id);
+        Employee GetEmployeeByName(string EmployeeName);
+        int GetEmployeeDepartmentId(int EmployeeId);
+    }
+}
