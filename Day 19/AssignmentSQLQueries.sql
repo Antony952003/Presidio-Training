4c05ec789bfe10e3d52c199a04ace77f69f5e98f
Antony952003 <antonyjeson2003@gmail.com>
2024-05-07 17:28:35 +0530

    final commit for the assignment

--- Day 19/AssignmentSQLQueries.sql
+++ Day 19/AssignmentSQLQueries.sql
@@ -1,7 +1,9 @@
 use pubs
+select * from pub_info
 select * from authors
 select * from titles
 select * from titleauthor
+select * from sales
 --1) Create a stored procedure that will take the author firstname and print all the books 
 --polished by him with the publisher's 
 create proc PrintAllBooks_procedure(@authorname varchar(20))
@@ -19,17 +21,19 @@ exec PrintAllBooks_procedure 'Sylvia'
 --2) Create a sp that will take the employee's firtname and print all the titles sold by him/her, 
 --price, quantity and the cost.
  select * from employee
- create proc PrintTitledetails_procedure(@ename varchar(20))
+create proc PrintTitledetails_procedure(@ename varchar(20))
 as
 begin
-	select title 
+	select title, t.price,s.qty
 	from employee e
 	join titles t on e.pub_id = t.pub_id
+	join sales s on s.title_id = t.title_id
 	where e.fname = @ename
 end
 
 exec PrintTitledetails_procedure 'Paul'
 
+drop proc PrintTitledetails_procedure
 --3) Create a query that will print all names from authors and employees
 
 select au_fname from authors
